44c998f4e8cde0a183538f6c48601ae03e712b0a
Paul Milne <hallhill@gmail.com>
2016-11-02 22:08:58 +0000

    Added methods to return different outcomes when calling the different strategies.

--- DealCanasta.java
+++ DealCanasta.java
@@ -1,6 +1,6 @@
 public class DealCanasta implements Deal {
      @Override
-     public int drawPile (int numberOfHands);{
+     public int drawPile (int numberOfHands){
           return "Draw pile for Canasta is " + (108 - (numberOfHands * 13)) + " cards".;
      }
 }
--- GamePatternDemo.java
+++ GamePatternDemo.java
@@ -1,1 +1,14 @@
-public class GamePatternDemo {}
+public class GamePatternDemo {
+     public static main(String[] args) {
+          
+          Hand hand = new Hand(new DealBriscola());
+          System.out.println(hand.cardsDeal(5));
+
+          hand = newHand(newDealPoker());
+          System.out.println(hand.cardsDeal(6));
+
+          hand = new Hand(new DealCanasta());
+          System.out.println(hand.cardsDeal(4));
+     }
+
+}
