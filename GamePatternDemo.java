f5fe191dd5f0f195a92e3d961f62f3bc2d4dd178
Paul Milne <hallhill@gmail.com>
2016-11-03 07:06:27 +0000

    Ran the GamePatternDemo and confirmed the Strategy pattern worked.

--- GamePatternDemo.java
+++ GamePatternDemo.java
@@ -2,10 +2,10 @@ public class GamePatternDemo {
      public static void main(String[] args) {
 
           Hand hand = new Hand(new DealBriscola());
-          System.out.println("Draw pile is " + hand.cardsDeal(5) + " cards.");
+          System.out.println("Draw pile is " + hand.cardsDeal(4) + " cards.");
 
           hand = new Hand(new DealPoker());
-          System.out.println("Draw pile is " + hand.cardsDeal(6) + " cards.");
+          System.out.println("Draw pile is " + hand.cardsDeal(4) + " cards.");
 
           hand = new Hand(new DealCanasta());
           System.out.println("Draw pile is " + hand.cardsDeal(4) + " cards.");
